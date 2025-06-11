9845b702951f3dc1c53dbf8269477b14b7ad12ca
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2025-06-11 08:26:06 +0200

    Implementation of UsersService. The service provides a method to retrieve the list of users

---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+
+@Injectable()
+export class UsersService {
+  private readonly filePath = path.join(__dirname, '..', 'users.json');
+
+  getAllUsers() {
+    try {
+      const data = fs.readFileSync(this.filePath, 'utf8');
+      return JSON.parse(data);
+    } catch (error) {
+      throw new InternalServerErrorException('Could not load user data');
+    }
+  }
+}
